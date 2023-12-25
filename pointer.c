3fc1f2afae0b8d196f6be1d91e90b63b0655161e
Luke Saint <kelytonlucas@gmail.com>
2023-12-25 11:04:41 -0300

    Exercicio de ponteiros como array. Manipulação de memoria

---
+++ pointer.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int size = 3;
+
+    int *ptr = (int*)malloc(3*sizeof(int));
+    
+    if(ptr == NULL){
+        printf("erro na alocação de memoria. \n");
+        return 1;
+    }
+
+    for(int i = 0; i < size; i++){
+        printf("Digite um valor: ");
+        scanf("%d", &ptr[i]);
+        printf("\n");
+    }
+
+    printf("Os valores digitados foram: \n");
+    for(int i = 0; i < size; i++){
+        printf("%d \n", ptr[i]);
+    }
+
+
+    return 0;
+}
